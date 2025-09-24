37dc0a722aca9fee5fa941a9d7087264b5c13088
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-24 08:34:10 -0300

    Add files via upload

---
+++ Autoria02_Milena_2DDS/Autoria02_Milena_2DDS/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Programa - Classificação por idade
+
+Console.WriteLine("Digite sua idade:");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade < 0)
+{
+    Console.WriteLine("Idade inválida.");
+}
+else if (idade <= 12)
+{
+    Console.WriteLine("Você é uma CRIANÇA.");
+}
+else if (idade <= 17)
+{
+    Console.WriteLine("Você é um ADOLESCENTE.");
+}
+else if (idade <= 59)
+{
+    Console.WriteLine("Você é um ADULTO.");
+}
+else
+{
+    Console.WriteLine("Você é um IDOSO.");
+}
+
+
